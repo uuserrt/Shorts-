99805b76ae2a59c97f6d0ea7e2591ac14625ebf1
uuserrt <amnydav72yw@gmail.com>
2025-11-22 11:22:16 +0530

    Update render.js

--- api/render.js
+++ api/render.js
@@ -28,7 +28,7 @@ export default async function handler(req,res){
       method:"POST",
       headers:{
         "Content-Type":"application/json",
-        "x-api-key": process.env.J2_KEY
+        "x-api-key": process.env.710H2d0DlBRGInGQE62HrZeECkejPKSoFi6DeRIA
       },
       body:JSON.stringify(payload)
     });
